035cf6bda79ee6af3087179d3c205aa49995c00e
Matheus Lima <matheuslima.thedev@gmail.com>
2022-08-08 13:31:48 -0300

    added loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.scss'
+
+export default function Loading() {
+    return (
+        <div className="facespinner__box">
+            <div className="facespinner">
+                <div className="facespinner__eye"></div>
+            </div>
+        </div>
+    );
+}
